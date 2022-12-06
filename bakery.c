d18785692b2c58072854e1ecd318be7d2dbec00e
Anna Carvalho <ajcarvalho1201@gmail.com>
2022-12-06 14:23:31 -0500

    reorganized methods

--- bakery.c
+++ bakery.c
@@ -10,11 +10,14 @@
 
 #include "ingredients.h"
 
+
+int isAvailable(int isNeeded, int isAvailable);
+void mixDryAndWetIngredients(Recipe r, Bakery* bakery, int baker, int dishes, int pantry);
+void useStandMixer(int baker, int dishes, int standMixer);
+void bake(int oven, int baker); 
+
 void wait_semaphore(int semId);
 void signal_semaphore(int semId);
-
-void bake(int semId, int baker); 
-void mixDryAndWetIngredients(Recipe r, Bakery* bakery, int baker, int dishes, int pantry, int standMixer);
 
 void generateDefaultValues(Bakery *b);
 void adjustBakeryValues(int bakersCount, Bakery *b);
@@ -23,7 +26,7 @@ int main(int argv, char* argc[])
 {
     // baker info
     char name[40];
-    int temp; //for testing
+    int id;
     int dishCount = 0;
 
     int pantry = semget(IPC_PRIVATE, 1, 00600);
@@ -72,12 +75,16 @@ int main(int argv, char* argc[])
         if ((pid = fork()) != 0)    // parent process
         {
             // TODO: randomly assign baker name & recipe
-            temp = i + 1;
+            id = i + 1;
             break;
         }
     }
 
-    mixDryAndWetIngredients(chocolate_cake, bakery, temp, dishCount, pantry, standMixer);
+    // leave this in for now
+    if (id > bakersCount + 1)
+        return 0;
+
+    mixDryAndWetIngredients(chocolate_cake, bakery, id, dishCount, pantry);
 
     // Detach the shared memory segment
     shmdt(bakery); 
@@ -92,6 +99,105 @@ int main(int argv, char* argc[])
     semctl(standMixer, 0, IPC_RMID);
 
     return 0;
+}
+
+int isAvailable(int isNeeded, int isAvailable)
+{
+    if (isAvailable - isNeeded > -1)
+        return 1;
+
+    return 0;
+}
+
+void mixDryAndWetIngredients(Recipe r, Bakery* bakery, int baker, int dishes, int pantry) 
+{    
+    wait_semaphore(pantry);
+
+    if(!( isAvailable(r.dry_ingredients.flour, bakery->flour) &&
+            isAvailable(r.dry_ingredients.cocoa_powder, bakery->cocoa_powder) && 
+            isAvailable(r.dry_ingredients.baking_powder, bakery->baking_powder) && 
+            isAvailable(r.dry_ingredients.baking_soda, bakery->baking_soda) && 
+            isAvailable(r.dry_ingredients.salt, bakery->salt) && 
+            isAvailable(r.wet_ingredients.butter, bakery->butter) &&
+            isAvailable(r.wet_ingredients.sugar, bakery->sugar) &&
+            isAvailable(r.wet_ingredients.vanilla, bakery->vanilla) &&
+            isAvailable(r.wet_ingredients.eggs, bakery->eggs) &&
+            isAvailable(r.wet_ingredients.milk, bakery->milk) ))
+    {
+        printf("Baker #%d doesn't have enough ingredients to finish their recipe.\n", baker);
+
+        // Do something to terminate the process since it can't finish
+        return;
+    }
+
+    // Remove dry ingredients from bakery
+    bakery->flour = bakery->flour - r.dry_ingredients.flour;
+    bakery->cocoa_powder = bakery->cocoa_powder - r.dry_ingredients.cocoa_powder;
+    bakery->baking_powder = bakery->baking_powder - r.dry_ingredients.baking_powder;
+    bakery->baking_soda = bakery->baking_soda - r.dry_ingredients.baking_soda;
+    bakery->salt = bakery->salt - r.dry_ingredients.salt;
+
+    // Remove wet ingredients from bakery
+    bakery->butter = bakery->butter - r.wet_ingredients.butter;
+    bakery->sugar = bakery->sugar - r.wet_ingredients.sugar;
+    bakery->vanilla = bakery->vanilla - r.wet_ingredients.vanilla;
+    bakery->eggs = bakery->eggs - r.wet_ingredients.eggs;
+    bakery->milk = bakery->milk - r.wet_ingredients.milk;
+    
+    signal_semaphore(pantry);
+
+    printf("Baker #%d is mixing their dry ingredients.\n", baker);
+    sleep(1);
+    printf("Baker #%d is done mixing their dry ingredients and is mixing their wet ingredients.\n", baker);
+    sleep(1);
+
+    dishes += 2;
+}
+
+void useStandMixer(int baker, int dishes, int standMixer)
+{
+    wait_semaphore(standMixer);
+
+    printf("Baker #%d is done mixing their dry and wet ingredients together.\n", baker);
+    sleep(1);
+
+    signal_semaphore(standMixer);
+
+    dishes += 1;
+}
+
+void bake(int oven, int baker) 
+{
+    wait_semaphore(oven);
+
+    // TODO: print name not number
+    printf("Baker #%d is baking their pastry!\n", baker);
+    sleep(5);
+    printf("ding ding ding!\n");
+
+    signal_semaphore(oven);
+}
+
+void wait_semaphore(int semId)
+{
+    struct sembuf sbuf;
+
+    sbuf.sem_num = 0;
+    sbuf.sem_op = -1;
+    sbuf.sem_flg = 0;
+
+    semop(semId, &sbuf, 1);
+}
+
+void signal_semaphore(int semId)
+{
+    struct sembuf sbuf;
+
+    sbuf.sem_num = 0;
+    sbuf.sem_op = 1;
+    sbuf.sem_flg = 0;
+
+    semop(semId, &sbuf, 1);
 }
 
 void generateDefaultValues(Bakery *b)
@@ -135,100 +241,4 @@ void adjustBakeryValues(int bakersCount, Bakery *b)
     b->powdered_sugar += b->powdered_sugar * ingredientMult;
 
     // TODO: how to do counting semaphore?
-}
-
-int isAvailable(int isNeeded, int isAvailable)
-{
-    if (isAvailable - isNeeded > -1)
-        return 1;
-
-    return 0;
-}
-
-void bake(int semId, int baker) 
-{
-    wait_semaphore(semId);
-
-    // TODO: print name not number
-    printf("Baker #%d is baking their pastry!\n", baker);
-    sleep(5);
-    printf("ding ding ding!\n");
-
-    signal_semaphore(semId);
-}
-
-void mixDryAndWetIngredients(Recipe r, Bakery* bakery, int baker, int dishes, int pantry, int standMixer) 
-{    
-    wait_semaphore(pantry);
-
-    if(!( isAvailable(r.dry_ingredients.flour, bakery->flour) &&
-            isAvailable(r.dry_ingredients.cocoa_powder, bakery->cocoa_powder) && 
-            isAvailable(r.dry_ingredients.baking_powder, bakery->baking_powder) && 
-            isAvailable(r.dry_ingredients.baking_soda, bakery->baking_soda) && 
-            isAvailable(r.dry_ingredients.salt, bakery->salt) && 
-            isAvailable(r.wet_ingredients.butter, bakery->butter) &&
-            isAvailable(r.wet_ingredients.sugar, bakery->sugar) &&
-            isAvailable(r.wet_ingredients.vanilla, bakery->vanilla) &&
-            isAvailable(r.wet_ingredients.eggs, bakery->eggs) &&
-            isAvailable(r.wet_ingredients.milk, bakery->milk) ))
-    {
-        printf("Baker #%d doesn't have enough ingredients to finish their recipe.\n", baker);
-
-        // Do something to terminate the process since it can't finish
-        return;
-    }
-
-    // Remove dry ingredients from bakery
-    
-    bakery->flour = bakery->flour - r.dry_ingredients.flour;
-    bakery->cocoa_powder = bakery->cocoa_powder - r.dry_ingredients.cocoa_powder;
-    bakery->baking_powder = bakery->baking_powder - r.dry_ingredients.baking_powder;
-    bakery->baking_soda = bakery->baking_soda - r.dry_ingredients.baking_soda;
-    bakery->salt = bakery->salt - r.dry_ingredients.salt;
-    
-
-    // Remove wet ingredients from bakery
-    bakery->butter = bakery->butter - r.wet_ingredients.butter;
-    bakery->sugar = bakery->sugar - r.wet_ingredients.sugar;
-    bakery->vanilla = bakery->vanilla - r.wet_ingredients.vanilla;
-    bakery->eggs = bakery->eggs - r.wet_ingredients.eggs;
-    bakery->milk = bakery->milk - r.wet_ingredients.milk;
-    
-    signal_semaphore(pantry);
-
-    printf("Baker #%d is mixing their dry ingredients.\n", baker);
-    sleep(1);
-    printf("Baker #%d is done mixing their dry ingredients and is mixing their wet ingredients.\n", baker);
-    sleep(1);
-
-    wait_semaphore(standMixer);
-
-    printf("Baker #%d is done mixing their dry and wet ingredients together.\n", baker);
-    sleep(1);
-
-    signal_semaphore(standMixer);
-
-    dishes += 2;
-}
-
-void wait_semaphore(int semId)
-{
-    struct sembuf sbuf;
-
-    sbuf.sem_num = 0;
-    sbuf.sem_op = -1;
-    sbuf.sem_flg = 0;
-
-    semop(semId, &sbuf, 1);
-}
-
-void signal_semaphore(int semId)
-{
-    struct sembuf sbuf;
-
-    sbuf.sem_num = 0;
-    sbuf.sem_op = 1;
-    sbuf.sem_flg = 0;
-
-    semop(semId, &sbuf, 1);
 }
